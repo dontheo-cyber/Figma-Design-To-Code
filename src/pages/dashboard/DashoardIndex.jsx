fe0ea2c368ae33630a6b718cce6cc1438e191fdb
Paulsonbigi <adegbokanpaul@gmail.com>
2023-04-06 19:43:10 +0100

    feat: greetings component

--- src/pages/dashboard/DashoardIndex.jsx
+++ src/pages/dashboard/DashoardIndex.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../../container/Layout'
+import Greetings from '../../views/Greetings'
 
 const DashoardIndex = () => {
   return (
     <div>
         <Layout>
+          <Greetings/>
         <h3>DashoardIndex</h3>
         <h3>DashoardIndex</h3>
         <h3>DashoardIndex</h3>
